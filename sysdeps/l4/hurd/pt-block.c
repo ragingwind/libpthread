57e1f67e3757c66023abf912c302bb2a328eaebb
Neal H. Walfield <neal@gnu.org>
2008-01-17 01:12:44 +0000

    2008-01-17  Neal H. Walfield  <neal@gnu.org>
    	* sysdeps/l4/hurd/pt-block.c: New file.
    	* sysdeps/l4/hurd/pt-wakeup.c: Likewise.

---
+++ sysdeps/l4/hurd/pt-block.c
@@ -0,0 +1,30 @@
+/* Block a thread.  Viengoos version.
+   Copyright (C) 2008 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with the GNU C Library; see the file COPYING.LIB.  If not,
+   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.  */
+
+#include <pt-internal.h>
+
+#include <hurd/stddef.h>
+#include <hurd/futex.h>
+
+/* Block THREAD.  */
+void
+__pthread_block (struct __pthread *thread)
+{
+  futex_wait (&thread->threadid, thread->threadid);
+}
