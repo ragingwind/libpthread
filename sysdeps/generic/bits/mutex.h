3bcc540d4c6c59dbfa5958d1b59942f8b610bf5e
Thomas Schwinge <tschwinge@gnu.org>
2008-08-03 07:02:36 +0000

    Update copyright year.

--- sysdeps/generic/bits/mutex.h
+++ sysdeps/generic/bits/mutex.h
@@ -1,5 +1,8 @@
 /* Mutex type.  Generic version.
-   Copyright (C) 2000, 2002, 2005, 2006, 2007 Free Software Foundation, Inc.
+
+   Copyright (C) 2000, 2002, 2005, 2006, 2007, 2008
+     Free Software Foundation, Inc.
+
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
