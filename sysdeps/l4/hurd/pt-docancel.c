7597f8f42a0f4319c49d62f96ccf5e4b52846a11
Neal H. Walfield <neal@gnu.org>
2008-08-16 13:35:09 +0000

    hurd/
    2008-08-16  Neal H. Walfield  <neal@gnu.org>
    
    	* thread.h (thread_start_sp_ip): New function.
    
    libpthread/
    
    2008-08-16  Neal H. Walfield  <neal@gnu.org>
    
    	* sysdeps/l4/hurd/pt-docancel.c: New file.

---
+++ sysdeps/l4/hurd/pt-docancel.c
@@ -0,0 +1,41 @@
+/* Cancel a thread.  Viengoos version.
+   Copyright (C) 2002, 2007, 2008 Free Software Foundation, Inc.
+   This file is part of the GNU Hurd.
+
+   GNU Hurd is free software: you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   GNU Hurd is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with GNU Hurd.  If not, see
+   <http://www.gnu.org/licenses/>.  */
+
+#include <pthread.h>
+
+#include <pt-internal.h>
+
+static void
+call_exit (void)
+{
+  pthread_exit (0);
+}
+
+int
+__pthread_do_cancel (struct __pthread *p)
+{
+  assert (p->cancel_pending == 1);
+  assert (p->cancel_state == PTHREAD_CANCEL_ENABLE);
+
+  if (l4_is_thread_equal (l4_myself (), p->threadid))
+    call_exit ();
+  else
+    thread_start_sp_ip (p->object,
+			p->mcontext.sp, (uintptr_t) call_exit);
+  return 0;
+}
